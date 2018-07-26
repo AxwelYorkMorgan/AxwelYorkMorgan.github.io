7cc17fdf5f1f296fba44eefa7c451bf54fd9fbda
Rustamov Ruslan <41525412+AxwelYorkMorgan@users.noreply.github.com>
2018-07-26 13:27:04 +0300

    v1.0.5

--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -1,13 +1,6 @@
-$(document).on('click', 'a[href^="#top"]', function (event) {
-    event.preventDefault();
-
-    $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 500);
-});
-
 $(document).ready(function () {
     let scrollSens = 100;
+    
     $(window).scroll(function () {
         if ($(this).scrollTop() > scrollSens) {
             $('#go-top').addClass('show');
@@ -15,4 +8,15 @@ $(document).ready(function () {
             $('#go-top').removeClass('show');;
         }
     });
+
+    $('#open-menu').click(function () {
+        alert("WIP");
+    });
+});
+
+$(document).on('click', 'a[href^="#top"]', function (event) {
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
 });
